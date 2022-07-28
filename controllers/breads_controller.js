4b6a53fbdf800c565a097e88c796da6109ca307a
Bradford Knight <bradford.a.knight@gmail.com>
2022-07-27 21:18:37 -0400

    Added delete route; added edit route/view, but default values are not populating form

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,6 +32,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:arrayIndex/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.arrayIndex],
+    index: [req.params.arrayIndex]
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -42,6 +50,17 @@ breads.get('/:arrayIndex', (req, res) => {
   } else {
     res.send('404')
   }
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
